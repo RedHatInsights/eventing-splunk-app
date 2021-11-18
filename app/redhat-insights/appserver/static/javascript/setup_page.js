15f6e9895145dbbb2116b0f2a1babcb83bf2a630
Viliam Krizan <vkrizan@redhat.com>
2021-11-18 18:24:35 +0100

    Splunk: create example setup page
    EVNT-82

---
+++ app/redhat-insights/appserver/static/javascript/setup_page.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var app_name = "redhat-insights";
+
+require.config({
+  paths: {
+    myApp: "../app/" + app_name + "/javascript/views/app",
+    react: "../app/" + app_name + "/javascript/vendor/react.production.min",
+    ReactDOM: "../app/" + app_name + "/javascript/vendor/react-dom.production.min",
+  },
+  scriptType: "module",
+});
+
+require([
+  "react",
+  "ReactDOM",
+  "myApp",
+], function (_react, ReactDOM, myApp) {
+  ReactDOM.render(myApp, document.getElementById('main_container'));
+})
+
