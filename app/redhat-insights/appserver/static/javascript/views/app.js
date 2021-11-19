d88f44132ca3b6c523ce2785b340224ff8cc3d32
Viliam Krizan <vkrizan@redhat.com>
2021-11-19 18:03:02 +0100

    Splunk: update setup form
    Adds more inputs and updates the form to comply with Splunk internal
    syle guide.
    
    Reference: http://localhost:8000/en-US/static/docs/style/style-guide.html
    
    EVNT-82

--- app/redhat-insights/appserver/static/javascript/views/app.js
+++ app/redhat-insights/appserver/static/javascript/views/app.js
@@ -4,10 +4,12 @@ define(["react", "splunkjs/splunk"], function(react, splunk_js_sdk){
   const e = react.createElement;
 
   const SetupPage = () =>  {
+    const [username, setUsername] = react.useState();
     const [password, setPassword] = react.useState();
+    const [cluster, setCluster] = react.useState("console.redhat.com");
 
-    const handleChange = (event) => {
-      setPassword(event.target.value);
+    const handleChangeFor = (setter) => (event) => {
+      setter(event.target.value);
     };
 
     const handleSubmit = async (event) => {
@@ -17,14 +19,43 @@ define(["react", "splunkjs/splunk"], function(react, splunk_js_sdk){
     }
 
     return e("div", null, [
-      e("h2", null, "Create a password to complete app setup."),
+      e("h2", null, "Set up the integration with Red Hat"),
       e("div", null, [
         e("form", { onSubmit: handleSubmit }, [
-          e("label", null, [
-            "Password ",
-            e("input", { type: "password", name: "password", value: password, onChange: handleChange })
+          e('fieldset', null, [
+            e('div', { class: 'form form-horizontal' }, [
+              e('div', { class: 'control-group shared-controls-controlgroup control-group-default' }, [
+                e('label', { class: 'control-label' }, ['Username']),
+                e('div', { class: 'controls controls-join' }, [
+                  e('div', { class: 'control shared-controls-textcontrol control-default' }, [
+                    e("input", { type: "text", name: "username", value: username, onChange: handleChangeFor(setUsername) })
+                  ])
+                ])
+              ]),
+              e('div', { class: 'control-group shared-controls-controlgroup control-group-default' }, [
+                e('label', { class: 'control-label' }, ['Password']),
+                e('div', { class: 'controls controls-join' }, [
+                  e('div', { class: 'control shared-controls-textcontrol control-default' }, [
+                    e("input", { type: "password", name: "password", value: password, onChange: handleChangeFor(setPassword) })
+                  ])
+                ])
+              ]),
+              e('div', { class: 'control-group shared-controls-controlgroup control-group-default' }, [
+                e('label', { class: 'control-label' }, ['Console']),
+                e('div', { class: 'controls controls-join' }, [
+                  e('div', { class: 'control shared-controls-textcontrol control-default' }, [
+                    e("input", { type: "text", name: "cluster", value: cluster, onChange: handleChangeFor(setCluster) })
+                  ])
+                ])
+              ]),
+              e('div', { class: 'control-group shared-controls-controlgroup control-group-default' }, [
+                e('label', { class: 'control-label' }),
+                e('div', { class: 'controls controls-join' }, [
+                  e("input", { type: "submit", value: "Log in & setup", class: 'btn btn-primary' }),
+                ])
+              ]),
+            ])
           ]),
-          e("input", { type: "submit", value: "Submit" })
         ])
       ])
     ]);
