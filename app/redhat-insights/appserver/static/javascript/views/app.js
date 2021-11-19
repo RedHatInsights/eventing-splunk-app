fa5e41d48cc520579e4aeb07de17cfab4dec0833
Viliam Krizan <vkrizan@redhat.com>
2021-11-19 14:13:48 +0100

    Splunk: Refactor SetupPage to be functional
    EVNT-82

--- app/redhat-insights/appserver/static/javascript/views/app.js
+++ app/redhat-insights/appserver/static/javascript/views/app.js
@@ -3,45 +3,32 @@ import * as Setup from "./store_secret.js";
 define(["react", "splunkjs/splunk"], function(react, splunk_js_sdk){
   const e = react.createElement;
 
-  class SetupPage extends react.Component {
-    constructor(props) {
-      super(props);
+  const SetupPage = () =>  {
+    const [password, setPassword] = react.useState();
 
-      this.state = {
-        password: ''
-      };
+    const handleChange = (event) => {
+      setPassword(event.target.value);
+    };
 
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-      this.setState({ ...this.state, [event.target.name]: event.target.value})
-    }
-
-    async handleSubmit(event) {
+    const handleSubmit = async (event) => {
       event.preventDefault();
 
-      await Setup.perform(splunk_js_sdk, this.state)
+      await Setup.perform(splunk_js_sdk, { password })
     }
 
-
-    render() {
-      return e("div", null, [
-        e("h2", null, "Create a password to complete app setup."),
-        e("div", null, [
-          e("form", { onSubmit: this.handleSubmit }, [
-            e("label", null, [
-              "Password ",
-              e("input", { type: "password", name: "password", value: this.state.password, onChange: this.handleChange })
-            ]),
-            e("input", { type: "submit", value: "Submit" })
-          ])
+    return e("div", null, [
+      e("h2", null, "Create a password to complete app setup."),
+      e("div", null, [
+        e("form", { onSubmit: handleSubmit }, [
+          e("label", null, [
+            "Password ",
+            e("input", { type: "password", name: "password", value: password, onChange: handleChange })
+          ]),
+          e("input", { type: "submit", value: "Submit" })
         ])
-      ]);
-    }
-
-  }
+      ])
+    ]);
+  };
 
   return e(SetupPage);
 });
