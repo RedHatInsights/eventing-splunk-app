043b6d170b46a19cb5dc61c0d3974396e3879214
Viliam Krizan <vkrizan@redhat.com>
2022-09-07 15:38:51 +0200

    fix(uuid): use library
    Refactoring of code to RequireJS with dependency injection was
    necessary.
    
    EVNT-349

--- app/redhat_insights/appserver/static/javascript/views/setup.js
+++ app/redhat_insights/appserver/static/javascript/views/setup.js
@@ -1,104 +1,110 @@
-"use strict";
 
-import * as Config from './setup_configuration.js'
+define(['./setup_configuration'], function(Config) {
 
-export const hecAndIndex = async (splunk_js_sdk, input) => {
-  var application_name_space = {
-    owner: "nobody",
-    app: app_name,
-    sharing: "app",
-  };
+  const hecAndIndex = async (splunk_js_sdk, input) => {
+    var application_name_space = {
+      owner: "nobody",
+      app: app_name,
+      sharing: "app",
+    };
 
-  try {
-    const service = Config.create_splunk_js_sdk_service(
-      splunk_js_sdk,
-      application_name_space,
-    );
-
-    return await Config.create_hec_collector(service, input);
-  } catch (error) {
-    throw new Error('Setup failed: ' + error);
-  }
-}
-
-export const complete = async (splunk_js_sdk) => {
-  var application_name_space = {
-    owner: "nobody",
-    app: app_name,
-    sharing: "app",
-  };
-
-  try {
-    const service = Config.create_splunk_js_sdk_service(
-      splunk_js_sdk,
-      application_name_space,
-    );
-
-    await Config.complete_setup(service);
-
-    await Config.reload_splunk_app(service, app_name);
-
-    Config.redirect_to_splunk_app_homepage(app_name);
-
-  } catch (error) {
-    throw new Error('Setup failed: ' + error);
-  }
-
-}
-
-export const getSplunkVersion = (splunk_js_sdk) => {
-  var application_name_space = {
-    owner: "nobody",
-    app: app_name,
-    sharing: "app",
-  };
-
-  return new Promise((resolve, reject) => {
     try {
       const service = Config.create_splunk_js_sdk_service(
         splunk_js_sdk,
         application_name_space,
       );
-      service.serverInfo((err, info) => {
-        if (err) {
-          reject(err);
-        }
-        else {
-          const value = info.properties().version
-          resolve(value);
-        }
-      })
-    } catch (err) {
-      reject(err);
+
+      return await Config.create_hec_collector(service, input);
+    } catch (error) {
+      throw new Error('Setup failed: ' + error);
     }
-  })
-}
+  }
 
-export const getAppVersion = (splunk_js_sdk) => {
-  var application_name_space = {
-    owner: "nobody",
-    app: app_name,
-    sharing: "app",
-  };
+  const complete = async (splunk_js_sdk) => {
+    var application_name_space = {
+      owner: "nobody",
+      app: app_name,
+      sharing: "app",
+    };
 
-  return new Promise((resolve, reject) => {
     try {
       const service = Config.create_splunk_js_sdk_service(
         splunk_js_sdk,
         application_name_space,
       );
-      const apps = service.apps();
-      apps.fetch((err, apps) => {
-        if (err) {
-          reject(err);
-        }
-        else {
-          var app = apps.item(app_name);
-          resolve(app._properties.version);
-        }
-      });
-    } catch (err) {
-      reject(err);
+
+      await Config.complete_setup(service);
+
+      await Config.reload_splunk_app(service, app_name);
+
+      Config.redirect_to_splunk_app_homepage(app_name);
+
+    } catch (error) {
+      throw new Error('Setup failed: ' + error);
     }
-  })
-}
+
+  }
+
+  const getSplunkVersion = (splunk_js_sdk) => {
+    var application_name_space = {
+      owner: "nobody",
+      app: app_name,
+      sharing: "app",
+    };
+
+    return new Promise((resolve, reject) => {
+      try {
+        const service = Config.create_splunk_js_sdk_service(
+          splunk_js_sdk,
+          application_name_space,
+        );
+        service.serverInfo((err, info) => {
+          if (err) {
+            reject(err);
+          }
+          else {
+            const value = info.properties().version
+            resolve(value);
+          }
+        })
+      } catch (err) {
+        reject(err);
+      }
+    })
+  }
+
+  const getAppVersion = (splunk_js_sdk) => {
+    var application_name_space = {
+      owner: "nobody",
+      app: app_name,
+      sharing: "app",
+    };
+
+    return new Promise((resolve, reject) => {
+      try {
+        const service = Config.create_splunk_js_sdk_service(
+          splunk_js_sdk,
+          application_name_space,
+        );
+        const apps = service.apps();
+        apps.fetch((err, apps) => {
+          if (err) {
+            reject(err);
+          }
+          else {
+            var app = apps.item(app_name);
+            resolve(app._properties.version);
+          }
+        });
+      } catch (err) {
+        reject(err);
+      }
+    })
+  }
+
+  return {
+    hecAndIndex, complete, getSplunkVersion, getAppVersion
+  };
+
+
+});
--- app/redhat_insights/appserver/static/javascript/views/util.js
+++ app/redhat_insights/appserver/static/javascript/views/util.js
@@ -1,10 +1,4 @@
-export function uuidv4() {
-  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-  );
-}
-
-export const validate_uuidv4 = (uuid) => 
+export const validate_uuidv4 = (uuid) =>
   /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i.test(uuid);
 
 export const promisify = (fn) => {
